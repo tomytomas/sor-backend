08a6ffea6fbfa246b250a350d88277d56b64d558
santiagotomas7474 <110138103+santiagotomas7474@users.noreply.github.com>
2025-02-04 14:09:25 -0300

    nuevo puerto

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ import express from "express";
 import cors from "cors";
 import 'dotenv/config'
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080; 
 const SERVER_URL = process.env.URL || "http://localhost";
 import session from "express-session";
 import productsRouter from "./Routes/products.js";
